1b2b47487959e1faa26390aa9982415d0f82ac54
isees <19368794+isees@users.noreply.github.com>
2020-05-07 21:35:11 +0800

    update files

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# 错误时终止脚本
+set -e
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+rm -rf public
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+cp CNAME public/
+
 # Go To Public folder
 cd public
 # Add changes to git.
+git init
 git add .
 
 # Commit changes.
@@ -18,7 +26,7 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push --force origin master
+git push -f git@github.com:isees/isees.github.io.git master
 
 # Come Back up to the Project Root
 cd ..
